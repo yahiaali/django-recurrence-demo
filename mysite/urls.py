2908ad096d4267ca95d4b077bad5f6abad78e5d8
Yahia Ali <yahiaali124@gmail.com>
2018-06-25 15:40:11 -0400

    Import include()

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 
 js_info_dict = {
