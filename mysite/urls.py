74405e35cbfdc813163eb83afb011c9596d3683f
Yahia Ali <yahiaali124@gmail.com>
2018-06-26 08:05:36 -0400

    Remove unused js_info_dict

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,12 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.i18n import JavaScriptCatalog
 
-js_info_dict = {
-    'packages': ['recurrence'],
-}
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('testapp.urls')),
-    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), js_info_dict),
+    url(r'^jsi18n/$', JavaScriptCatalog.as_view(), name='javascript_catalog'),
 ]
