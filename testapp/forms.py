3ddee3f5cbf320af602669f7b32451a73233f6f7
Yahia Ali <yahiaali124@gmail.com>
2018-06-26 07:37:08 -0400

    Include required javascript and css files manually

--- testapp/forms.py
+++ testapp/forms.py
@@ -5,4 +5,11 @@ from .models import Course
 class CourseForm(forms.ModelForm):
     class Meta:
         model = Course
-        fields = ('title', 'recurrences',)
+        fields = '__all__'
+
+    class Media:
+        js = ('recurrence/js/recurrence.js',
+              'recurrence/js/recurrence-widget.js')
+        css = {
+            'all': ('recurrence/css/recurrence.css',)
+        }
